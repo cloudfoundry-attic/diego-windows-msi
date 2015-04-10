052014d8ae986c9da8f00ac169005e5f8bee7850
Ben Moss and David Morhovich <pair+bmoss+dmorhovich@pivotal.io>
2015-04-10 15:34:45 -0400

    stop installing bundler

--- scripts/deploy_msi.sh
+++ scripts/deploy_msi.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-# chruby 2.1.2
-gem install bundler --no-rdoc --no-ri
 export BUNDLE_GEMFILE=$(dirname $0)/Gemfile
 mkdir -p $(dirname $0)/vendor/cache
 bundle install --path $(dirname $0)vendor/cache
