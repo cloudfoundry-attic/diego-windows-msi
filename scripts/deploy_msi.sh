07b66f0f36df42ccaec2e985a414e5c6b70fd656
Ben Moss and David Morhovich <pair+bmoss+dmorhovich@pivotal.io>
2015-04-10 17:00:08 -0400

    wip

--- scripts/deploy_msi.sh
+++ scripts/deploy_msi.sh
@@ -5,4 +5,4 @@ env
 export BUNDLE_GEMFILE=$(dirname $0)/Gemfile
 mkdir -p $(dirname $0)/../vendor/cache
 bundle install --path $(dirname $0)/../vendor/cache
-bundle exec $(dirname $0)/deploy_msi.rb "$@"
+bundle exec $(dirname $0)/deploy_msi.rb $(basename $@)
