14dabea8f005d1cdfbd4736c49b777bb5c8f7100
Dave_Goddard <dgoddard@pivotal.io>
2015-05-08 17:05:53 -0400

    WIP

--- scripts/make.bat
+++ scripts/make.bat
@@ -26,22 +26,6 @@ go install github.com/coreos/etcd || exit /b 1
 go install github.com/onsi/ginkgo/ginkgo || exit /b 1
 go install github.com/onsi/gomega || exit /b 1
 
-SET GOBIN=%CD%\DiegoWindowsMSI\DiegoWindowsMSI\go-executables
-
-:: Install metron, it contains all relevant gocode inside itself.
-pushd src\github.com\cloudfoundry\loggregator || exit /b 1
-  SET OLD_GOPATH=%GOPATH%
-  SET GOPATH=%CD%
-  go install metron || exit /b 1
-  SET GOPATH=%OLD_GOPATH%
-popd
-
-:: Install the garden-windows, rep and executor in the MSI go-executables directory
-go install github.com/cloudfoundry-incubator/garden-windows || exit /b 1
-go install github.com/cloudfoundry-incubator/executor/cmd/executor || exit /b 1
-go install github.com/cloudfoundry-incubator/rep/cmd/rep || exit /b 1
-copy bin\consul.exe %GOBIN%
-
 pushd src\github.com\cloudfoundry-incubator\garden-windows\containerizer || exit /b 1
   call make.bat || exit /b 1
 popd
@@ -55,18 +39,7 @@ ginkgo -skip=reports.garden.containers.as.-1  -r -noColor src/github.com/cloudfo
 ginkgo -skip=when.an.interrupt.signal.is.sent.to.the.representative^|should.not.exit,.but.keep.trying.to.maintain.presence.at.the.same.ID^|The.Rep.Evacuation.when.it.has.running.LRP.containers^|when.a.Ping.request.comes.in -noColor src/github.com/cloudfoundry-incubator/rep || exit /b 1
 
 
-for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
-    set VERSION=%%a
-)
-
-pushd DiegoWindowsMSI || exit /b 1
-  rmdir /S /Q packages
-  nuget restore || exit /b 1
-  echo SHA: %VERSION% > RELEASE_SHA
-  devenv DiegoWindowsMSI\DiegoWindowsMSI.vdproj /build "Release" || exit /b 1
-  xcopy DiegoWindowsMSI\Release\DiegoWindowsMSI.msi ..\output\ || exit /b 1
-popd
-move /Y output\DiegoWindowsMSI.msi output\DiegoWindowsMSI-%VERSION%.msi || exit /b 1
+call scripts\make_msi.bat || exit /b 1
 
 pushd src\github.com\cloudfoundry-incubator\windows_app_lifecycle || exit /b 1
   call make.bat || exit /b 1
---
+++ scripts/make_msi.bat
@@ -0,0 +1,28 @@
+SET GOBIN=%CD%\DiegoWindowsMSI\DiegoWindowsMSI\go-executables
+
+:: Install metron, it contains all relevant gocode inside itself.
+pushd src\github.com\cloudfoundry\loggregator || exit /b 1
+  SET OLD_GOPATH=%GOPATH%
+  SET GOPATH=%CD%
+  go install metron || exit /b 1
+  SET GOPATH=%OLD_GOPATH%
+popd
+
+:: Install the garden-windows, rep and executor in the MSI go-executables directory
+go install github.com/cloudfoundry-incubator/garden-windows || exit /b 1
+go install github.com/cloudfoundry-incubator/executor/cmd/executor || exit /b 1
+go install github.com/cloudfoundry-incubator/rep/cmd/rep || exit /b 1
+copy bin\consul.exe %GOBIN%
+
+for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
+    set VERSION=%%a
+)
+
+pushd DiegoWindowsMSI || exit /b 1
+  rmdir /S /Q packages
+  nuget restore || exit /b 1
+  echo SHA: %VERSION% > RELEASE_SHA
+  devenv DiegoWindowsMSI\DiegoWindowsMSI.vdproj /build "Release" || exit /b 1
+  xcopy DiegoWindowsMSI\Release\DiegoWindowsMSI.msi ..\output\ || exit /b 1
+popd
+move /Y output\DiegoWindowsMSI.msi output\DiegoWindowsMSI-%VERSION%.msi || exit /b 1
