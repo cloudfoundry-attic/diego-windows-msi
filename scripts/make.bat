d0cce2a9304ebd43ac74b2c311405088e63e2a54
Stefan Schneider <stefan.schneider@hp.com>
2015-09-25 06:15:21 -0700

    RUn all rep tests

--- scripts/make.bat
+++ scripts/make.bat
@@ -67,7 +67,7 @@ ginkgo -r -noColor src/github.com/cloudfoundry-incubator/garden-windows || exit 
 :: windows cmd doesn't like quoting arguments, use -skip=foo.bar instead of -skip='foo bar'
 :: we use the dot operator to match anything, -skip expects a regex
 ginkgo -r -noColor src/github.com/cloudfoundry-incubator/executor || exit /b 1
-ginkgo -skip=when.an.interrupt.signal.is.sent.to.the.representative^|should.not.exit,.but.keep.trying.to.maintain.presence.at.the.same.ID^|The.Rep.Evacuation.when.it.has.running.LRP.containers^|when.a.Ping.request.comes.in -noColor src/github.com/cloudfoundry-incubator/rep || exit /b 1
+ginkgo -noColor src/github.com/cloudfoundry-incubator/rep || exit /b 1
 
 SET GOPATH=%CD%
 echo F | xcopy bin\generate.exe output\generate-%VERSION%.exe || exit /b 1
